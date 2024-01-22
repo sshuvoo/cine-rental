2b8d882082b63bb8241c9a4bda2ade49e9c7eeb8
sshuvoo <safayanshuvo@gmail.com>
2024-01-22 22:58:26 +0600

    Update Footer Message

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ export default function Footer() {
       <footer className="py-6 md:py-8 mt-8">
          <div className="container mx-auto">
             <p className="text-center text-sm text-black/30 dark:text-[#EEEEEE] ">
-               Copyright ©2024 | All rights reserved by Learn with Sumit
+               Copyright ©2024 | All rights reserved by LWS | Saffaullah Shuvo
             </p>
          </div>
       </footer>
